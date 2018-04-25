2ea8a7353d55ec04aea7add52f9b827aa4186f8b
halefall <jofe66@hotmail.fr>
2018-04-25 17:55:28 +0200

    Ajout de commentaires, correction array des partitions

--- partitions.c
+++ partitions.c
@@ -9,24 +9,29 @@ int main(int argc, char *argv[])
 {
 
 	FILE *fp;
-	
+
 	char *buffer;
 	size_t size = 0;
-	
+
+	/*Redirection de stderr sur stdout pour éviter l'affichage d'éventuels messages d'erreur*/
 	fp = popen("sinfo -o '%P' 2>&1", "r");
-	
+
+	/*lecture du résultat de la commande dans buffer*/
 	fseek(fp, 0L, SEEK_END);
 	size = ftell(fp);
-	
+
 	rewind(fp);
-	
+
 	buffer = calloc( 1, size+1);
 	fread(buffer, size, 1, fp);
-	int errcode = WEXITSTATUS(pclose(fp));
 
-	if (errcode != 0) {
+	/*ne renvoie 0 que lorsqu'il n'y a eu aucune erreur*/
+	if (WEXITSTATUS(pclose(fp) != 0) {
 		printf("slurm probably not present on this system\n");
 		return(-1);
+	}
+	else {
+		printf("Slurm detected\n");
 	}
 
 	/* Compte le nombre de retours à la ligne
@@ -37,24 +42,25 @@ int main(int argc, char *argv[])
 		i++;
 		pch=strchr(pch+1,'\n');
 	}
-	
+
 	/* Sépare le noms des partitions aux \n
-	dans un array pour pouvoir accéder aux noms (le titre de la colonne "PARTITIONS" aussi)*/
+	dans un array pour pouvoir accéder aux noms*/
 	int j=0;
 	char *p = strtok(buffer, "\n");
 	char *array[i];
-	
+
 	while (p != NULL) {
 		array[j++] = p;
 		p = strtok(NULL, "\n");
 	}
-	
-	/*Affiche les partitions (en omettant le titre)*/
-	
-	printf("slurm detected, %i available partitions:\n", i-1);
-	for (j=1; j<3;++j)
-		printf("%s\n", array[j]);
-			
+	char* partitionArr = array + 1;
+
+	/*Affiche les partitions*/
+
+	printf("%i available partitions:\n", i-1);
+	for (j=0; j<i;++j)
+		printf("%s\n", paritionArr[j]);
+
 	free(buffer);
 
 	return 0;
